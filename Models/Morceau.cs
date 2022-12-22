a0110c9b714029b7e1ef4377a39d2a6019b2cae3
timilyo80 <78914833+timilyo80@users.noreply.github.com>
2022-12-22 01:55:32 -0500

    initialisation
    dunno

---
+++ Models/Morceau.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class Morceau
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Artiste { get; set; }
+        public string Duree { get; set; }
+        public string Genre { get; set; }
+        public string Annee { get; set; }
+        public string Chemin { get; set; }
+
+    }
+}
